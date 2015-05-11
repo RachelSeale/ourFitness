3e6adbcb277017a2b9c7bbfd2d6b572f5e1fdb4b
Rachel Seale <rvseale@sky.com>
2015-05-11 23:39:02 +0100

    correcting footer

--- footer.php
+++ footer.php
@@ -13,6 +13,11 @@
         <div class="footer-info">
           <a href="mailto: enquires@ourfitness.com">Contact Us</a>
         </div>
+        <div class="footer-info flaticon">
+          <div>Icons made by <a href="http://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>
+        </div>
+
+
       </section>
   
     </footer>  
